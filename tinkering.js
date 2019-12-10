cb4a5d507292e3788b1c4fe9ab257c5c551da415
Eina Onting <einers@outlook.com>
2019-12-10 21:02:13 +0000

    Use a while loop to repeat a chorus line

---
+++ tinkering.js
@@ -0,0 +1,22 @@
+function multiply(a, b) {
+    return a * b;
+}
+
+const result = multiply(2, 4);
+
+console.log(result);
+
+// change the value of `education` to test your code
+var education = 'no high school diploma';
+
+// set the value of this based on a person's education
+var salary = 0;
+
+// your code goes here
+switch (education) {
+    case:
+        break;
+    default:
+        salary = 25,636;
+        console.log("In 2015, a person with " + education + " earned an average of $" + salary + "/year.");
+}
