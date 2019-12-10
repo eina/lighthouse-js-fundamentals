cde16ac5dece2db69b3522d5fe83d6ed28a7ed7a
Eina Onting <einers@outlook.com>
2019-12-10 22:02:15 +0000

    lighthouse loopy solution

--- tinkering.js
+++ tinkering.js
@@ -5,18 +5,3 @@ function multiply(a, b) {
 const result = multiply(2, 4);
 
 console.log(result);
-
-// change the value of `education` to test your code
-var education = 'no high school diploma';
-
-// set the value of this based on a person's education
-var salary = 0;
-
-// your code goes here
-switch (education) {
-    case:
-        break;
-    default:
-        salary = 25,636;
-        console.log("In 2015, a person with " + education + " earned an average of $" + salary + "/year.");
-}
