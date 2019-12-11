93d4a5e597661f272da9984a604663181722866b
Eina Onting <einers@outlook.com>
2019-12-11 21:04:34 +0000

    add ice cream and packing-list exercises

---
+++ ice-cream.js
@@ -0,0 +1,14 @@
+// Do not modify these first two lines
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+const moreFlavours = iceCreamFlavours.concat(["root beer"]);
+
+console.log(moreFlavours);
+
+console.log(iceCreamFlavours[0]);
+
+console.log(moreFlavours[moreFlavours.length -1]);
+
+console.log(moreFlavours.length);
