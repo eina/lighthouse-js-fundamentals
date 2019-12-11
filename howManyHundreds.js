ada864c7282a84d8079a865449db6049b13e4f1a
Eina Onting <einers@outlook.com>
2019-12-11 17:52:46 +0000

    create howManyHundreds

---
+++ howManyHundreds.js
@@ -0,0 +1,11 @@
+function howManyHundreds(num){
+    const output = num / 100;
+
+    return Math.floor(output); 
+}
+
+console.log(howManyHundreds(1000), "=?", 10);
+console.log(howManyHundreds(894), "=?", 8);
+console.log(howManyHundreds(520), "=?", 5);
+console.log(howManyHundreds(99), "=?", 0);
+console.log(howManyHundreds(0), "=?", 0);
